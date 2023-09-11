41cc1df5e741688299343313f430c411c5e95ebb
avelinojrr <axelavelino1@gmail.com>
2023-09-11 12:35:53 -0400

    refactor: code and function changes for better performance

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -3,42 +3,42 @@ import {STRIPE_APY_SECRET} from "../config.js"
 
 const stripe = new Stripe(STRIPE_APY_SECRET)
 export const checkoutPay = async (req, res) => {
-    await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'MacBook Pro 2022',
-                        description: 'MacBook Pro 2022 1TB SSD 32GB RAM 16 inch display Chip M2',
-                        images: ['https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=452&hei=420&fmt=jpeg&qlt=95&.v=1664497359481'],
+    try {
+        await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'MacBook Pro 2022',
+                            description: 'MacBook Pro 2022 1TB SSD 32GB RAM 16 inch display Chip M2',
+                            images: ['https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=452&hei=420&fmt=jpeg&qlt=95&.v=1664497359481'],
+                        },
+                        unit_amount: 120000, // Cents 1200 USD
                     },
-                    unit_amount: 120000, // Cents 1200 USD
+                    quantity: 2,
                 },
-                quantity: 2,
-            },
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'AirPods Pro',
-                        description: 'AirPods Pro 2021 with noise cancellation and spatial audio',
-                        images: ['https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-hero-select-202011?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1604709293000'],
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'AirPods Pro',
+                            description: 'AirPods Pro 2021 with noise cancellation and spatial audio',
+                            images: ['https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-hero-select-202011?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1604709293000'],
+                        },
+                        unit_amount: 45000, // Cents 450 USD
                     },
-                    unit_amount: 45000, // Cents 450 USD
-                },
-                quantity: 1,
-            }
-        ],
-        mode: 'payment',
-        success_url: 'http://localhost:7090/success',
-        cancel_url: 'http://localhost:7090/failed',
-    }).then((session) => {
-        res.json({ url: session.url })
-    })
+                    quantity: 1,
+                }
+            ],
+            mode: 'payment',
+            success_url: 'http://localhost:7090/success',
+            cancel_url: 'http://localhost:7090/failed',
+        }).then((session) => {
+            res.json({ url: session.url })
+        })
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 }
-
-export const successPay = (req, res) => res.redirect('/success.html')
-
-export const failedPay = (req, res) => res.redirect('/failed.html')
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { checkoutPay, failedPay, successPay } from "../controllers/payment.controllers.js";
+import { checkoutPay } from "../controllers/payment.controllers.js";
 
 const router = Router();
 
 router.post('/create-checkout-session', checkoutPay);
 
-router.get('/success', successPay);
+router.get('/success',(req, res) => res.redirect('/success.html'));
 
-router.get('/failed', failedPay);
-
+router.get('/failed', (req, res) => res.redirect('/failed.html'));
 
 export default router;
